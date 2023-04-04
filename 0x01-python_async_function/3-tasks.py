8369c39e197687459a4c3e8d2c6012763641b5b9
gims-inc <gabrielmusanga@gmail.com>
2023-04-04 17:05:27 +0300

    regular async

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""use the regular function syntax to create an async fxn"""
+import asyncio
+from asyncio import Task
+from typing import Awaitable
+
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> Awaitable[Task]:
+    """regular function that takes an integer max_delay and
+    returns a asyncio.Task.
+
+    max_delay: int
+    returns asyncio.Task
+    """
+    return asyncio.create_task(wait_random(max_delay))
