59d051647facf1a59fbefdfcab68a48d3bc20313
gims-inc <gabrielmusanga@gmail.com>
2023-04-05 02:44:17 +0300

    Async Comprehensions

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Task 2 --Async Comprehensions"""
+import asyncio
+
+
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension():
+    """coroutine, collects 10 random numbers using an async
+    comprehensing over async_generator
+
+    return the 10 random numbers
+    """
+    return [i async for i in async_generator()]
