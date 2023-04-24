d1246299f3b4c837086cf886b9127ce7f5c33890
gims-inc <gabrielmusanga@gmail.com>
2023-04-24 17:26:40 +0300

    test memoize

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Test Suite for utils.py
 """
-from utils import access_nested_map, get_json
+from utils import access_nested_map, get_json, memoize
 import unittest
 from typing import Dict
 from unittest.mock import patch, Mock
@@ -49,3 +49,33 @@ class TestGetJson(unittest.TestCase):
         mock_requests.get.return_value = mock_resp
         result = get_json(turl)
         self.assertEqual(result, expected_json)
+
+
+class TestMemoize(unittest.TestCase):
+    """Test suite for memoize decorator"""
+
+    def test_memoize(self):
+        """ Test that when calling a_property twice, the correct
+        result is returned but a_method is only called once using
+        assert_called_once.
+        """
+        class TestClass:
+            """Class for testing the decorator"""
+
+            def a_method(self):
+                """method that always returns 42"""
+                return 42
+
+            @memoize
+            def a_property(self):
+                """decorated class property that calls a method"""
+                return self.a_method()
+
+        with patch.object(TestClass, 'a_method', return_value=42) as patcher:
+            testinstance = TestClass()
+
+            result = testinstance.a_property
+            result = testinstance.a_property
+
+            self.assertEqual(result, 42)
+            patcher.assert_called_once()
